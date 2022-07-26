ecda39cb868fde14bff14b91071ed691f303a036
r m <ribhav20@gmail.com>
2022-07-25 17:48:33 -0700

    commit

---
+++ classifier.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from PIL import Image
+
+X = np.load('image.npz') ['arr_0']
+y = pd.read_csv ("labels.csv") ["labels"]
+
+classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', "K", "L", "M", "N", "O", "P", "0", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+nclasses = len(classes)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=9, train_size=3500, test_size=500)
+
+clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train, y_train)
+
+def get_prediction(image):
+
+    im_pil = Image.open(image)  
+    
+    image_bw = im_pil.convert('L')
+    image_bw_resized = image_bw.resize((22,30), Image.ANTIALIAS)
+
+    pixel_filter = 20
+    min_pixel = np.percentile(image_bw_resized, pixel_filter)
+    max_pixel = np.max(image_bw_resized)
+
+    image_bw_resized_inverted_scaled = np.clip(image_bw_resized-min_pixel, 0, 255)
+    image_bw_resized_inverted_scaled = np.asarray(image_bw_resized_inverted_scaled)/max_pixel
+
+    test_sample = np.array(image_bw_resized_inverted_scaled).reshape(1,660)
+    test_pred = clf.predict(test_sample)
+
+    return test_pred[0]
